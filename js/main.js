ae59de3d756f2678fcde4d5234db717bcb0c6929
whiteEuphoria <prognikitos@gmail.com>
2022-01-20 10:08:10 +0200

    Full version

--- js/main.js
+++ js/main.js
@@ -4,10 +4,45 @@ const sidebar = document.querySelector('.header__slider--sidebar')
 const container = document.querySelector('.header__slider--container')
 const mainSlide = document.querySelector('.header__slider--main-slide')
 const slidesCount = mainSlide.querySelectorAll('div').length
+const burgerBtn = document.querySelector(".burger")
+const removeClass = document.querySelector(".nav-menu")
+
+
+burgerBtn.addEventListener("click", function (){
+    document.querySelector(".menu").classList.toggle("menu--visible");
+    document.querySelector(".header__slider--controls").classList.toggle("unvis");
+    document.querySelector(".logo").classList.toggle("fixed");
+    document.querySelector(".burger").classList.toggle("burger--active");
+});
+
+removeClass.addEventListener("click", function (){
+    document.querySelector(".menu").classList.remove("menu--visible");
+    document.querySelector(".header__slider--controls").classList.remove("unvis");
+    document.querySelector(".logo").classList.remove("fixed");
+    document.querySelector(".burger").classList.remove("burger--active");
+});
 
 let activeSlideIndex = 0
 
-sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+// sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+window.addEventListener('resize', function(event){
+  const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  if(viewport_width <= 768){
+    sidebar.style.top = `-${(slidesCount - 1) * 70}vh`
+  } else if(viewport_width >= 768){
+    sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+  }
+})
+
+window.addEventListener('load', function(event){
+  const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  if(viewport_width <= 768){
+    sidebar.style.top = `-${(slidesCount - 1) * 70}vh`
+  } else if(viewport_width >= 768){
+    sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+  }
+})
 
 upBtn.addEventListener('click', () => {
   changeSlide('up')
@@ -23,6 +58,15 @@ document.addEventListener('keydown', event => {
     changeSlide('down')
   }
 })
+
+document.addEventListener('keydown',event => {
+  if(event.key === 'ArrowUp') {
+    changeSlide('up')
+  }else if(event.key == 'ArrowDown'){
+    changeSlide('down')
+  }
+})
+
 
 function changeSlide(direction){
   if (direction === 'up') {
